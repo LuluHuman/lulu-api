e9eda5fc93ec721753c5832e8753b1a79b213e6d
Lulu <lutfilkhaliq587@gmail.com>
2026-03-15 11:07:21 +0800

    change redirect address

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { permanentRedirect, RedirectType } from "next/navigation";
 
 export default function Home() {
-    permanentRedirect("https://github.com/LuluHuman/rurutbl-react", RedirectType.replace);
+    permanentRedirect("https://luluhoy.tech", RedirectType.replace);
 }
